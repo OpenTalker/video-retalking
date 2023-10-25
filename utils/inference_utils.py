b2453a3a4811348375883b8f60b5ecd67852a045
glovebx <glovebx@glovebxdeMacBook-Pro-2.local>
2023-10-25 10:18:11 +0800

    Compatible with CPU mode, now it can run on Mac M1 computers

--- utils/inference_utils.py
+++ utils/inference_utils.py
@@ -107,7 +107,7 @@ def get_smoothened_boxes(boxes, T):
 
 def face_detect(images, args, jaw_correction=False, detector=None):
     if detector == None:
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
         detector = face_detection.FaceAlignment(face_detection.LandmarksType._2D, 
                                                 flip_input=False, device=device)
 
